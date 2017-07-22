7612fe486d05048adcaa2dedce9ca680b9eabca9
Pogány János <pogany.janos@gmail.com>
2017-07-22 10:39:39 +0200

    added UShape

---
+++ src/main/java/Tetris/Shapes/UShape.java
@@ -0,0 +1,28 @@
+package Tetris.Shapes;
+
+import Tetris.Position;
+
+/**
+ * Az {@code UShape} osztálya, melynek konstruktorában van megadva, hogy hogyan nézzen ki a mátrixa.
+ * 
+ * @see Shape
+ */
+public class UShape extends Shape
+{
+	/**
+	 * A {@code UShape} mátrixbeli reprezentációja. Az alakzat az ősosztály
+	 * konstruktorának a meghívásával kapja meg a pozíciót.
+	 * 
+	 * @param position az alakzat pozíciója
+	 * @see Position
+	 */
+	public UShape(Position position)
+	{
+		super(position);	//ősosztály konstruktorát meghívom
+		this.matrix = new int[][]{
+			{8, 0, 8}, 
+			{8, 8, 8}, 
+			{0, 0, 0}};	//tényleges mátrix
+	}
+}
+
